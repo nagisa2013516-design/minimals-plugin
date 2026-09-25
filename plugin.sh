63840232dfde01754d166bc84b07293a99f143d9
nagisa2013516-design <nagisa2013516@gmail.com>
2026-09-26 03:00:56 +0900

    Add header and success messages to plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -1,1 +1,8 @@
-
+#!/bin/sh
+clear
+echo "==============================================="
+echo "       SUCCESS: CUSTOM MIMALS PLUGIN v1.0      "
+echo "==============================================="
+echo "Hello World! This code was downloaded from GitHub."
+echo "Minimals Linux is successfully extended by Nagisa!"
+echo "==============================================="
